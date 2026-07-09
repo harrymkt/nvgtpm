64c9abd21ed0978b7be1310e8d117a5efe4d2f82
Harry Min Khant <118270468+harrymkt@users.noreply.github.com>
2026-07-09 12:41:51 +0630

    Fix execution of os.execv

--- src/program/__init__.py
+++ src/program/__init__.py
@@ -53,7 +53,7 @@ def update_cmd(args):
 			old_exe = f"{current_exe}.old"
 			os.replace(current_exe, old_exe)
 			os.replace(tmp_path, current_exe)
-			os.execv(current_exe, "[applyupdatesilently]")
+			os.execv(current_exe, ["[applyupdatesilently]"])
 		else:
 			os.chmod(tmp_path, os.stat(tmp_path).st_mode | stat.S_IEXEC)
 			shutil.move(tmp_path, current_exe)
